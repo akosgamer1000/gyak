925a7023bccaa398316aa335a4544e212d2bea8f
Petrik Diak <diak@petrik.hu>
2024-10-24 14:34:34 +0200

    valid

--- src/book/dto/create-book.dto.ts
+++ src/book/dto/create-book.dto.ts
@@ -1,6 +1,17 @@
+import { IsEmpty, IsNotEmpty, IsNumber, IsPositive, IsString } from "class-validator"
+
 export class CreateBookDto {
+  @IsString()
+  @IsNotEmpty()
  title: string
+ @IsString()
+ @IsNotEmpty()
  author: string
+ @IsString()
+ @IsNotEmpty()
  isbn: string
+ @IsNumber()
+ @IsPositive()
+ @IsNotEmpty()
  publishYear: number
 }
--- src/book/dto/update-book.dto.ts
+++ src/book/dto/update-book.dto.ts
@@ -1,6 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBookDto } from './create-book.dto';
+import { IsBoolean, IsEmpty, IsNotEmpty } from 'class-validator';
 
 export class UpdateBookDto extends PartialType(CreateBookDto) {
+  @IsBoolean()
+  @IsNotEmpty()
   reserved:boolean
 }
